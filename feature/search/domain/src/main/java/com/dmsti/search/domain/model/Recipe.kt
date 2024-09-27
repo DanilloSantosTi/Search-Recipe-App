812e905109cbe203b3ec629b7557d52c5cf5c839
Danillo Santos <danillomdsti@gmail.com>
2024-09-27 02:48:13 -0300

    feature/create_doamin_layer feature - inital commit domain

---
+++ feature/search/domain/src/main/java/com/dmsti/search/domain/model/Recipe.kt
@@ -0,0 +1,12 @@
+package com.dmsti.search.domain.model
+
+data class Recipe(
+    val idMeal: String,
+    val strArea: String,
+    val strMeal: String,
+    val strMealThumb: String,
+    val strCategory: String,
+    val strTags: String,
+    val strYoutube: String,
+    val strInstructions: String,
+)
